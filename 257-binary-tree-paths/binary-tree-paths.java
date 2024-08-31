82d2c68c103613647e2597c507876f96b4e12a65
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-08-31 18:22:36 +0500

    Time: 3 ms (64.29%) | Memory: 42.5 MB (65.92%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -14,22 +14,36 @@
  * }
  */
 class Solution {
-    public void paths(TreeNode root, String str, List<String> res){
+    String listToString(List<Integer> list){
+        StringBuilder str = new StringBuilder();
+        str.append(list.get(0));
+        for(int i=1; i<list.size(); i++){
+            str.append("->" + list.get(i));
+        }
+        return str.toString();
+    }
+    public void paths(TreeNode root, List<Integer> list, List<String> res){
         if(root == null){
             return;
         }
         if(root.left == null && root.right == null){
-            res.add(str + root.val);
+            list.add(root.val);
+            res.add(listToString(list));
+            list.remove(list.size()-1);
             return;
         }
         
-        paths(root.left, str + root.val + "->", res);
-        paths(root.right, str + root.val + "->", res);
+        list.add(root.val);
 
+        paths(root.left, list, res);
+        paths(root.right, list, res);
+        
+        list.remove(list.size()-1);
     }
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
-        paths(root, "", res);
+        List<Integer> list = new ArrayList<>();
+        paths(root, list, res);
         return res;
     }
 }
