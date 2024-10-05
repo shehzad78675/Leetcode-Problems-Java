2bcb356286fd41ef796d6e47a9d48471af71d312
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-05 09:55:45 +0500

    Time: 27 ms (10.91%) | Memory: 44.7 MB (7.18%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solve(int n, int[] dp){
+        if(n <= 1){
+            return n;
+        }
+
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        return dp[n] = fib(n-1) + fib(n-2);
+    }
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        
+        return solve(n, dp);
+    }
+}
