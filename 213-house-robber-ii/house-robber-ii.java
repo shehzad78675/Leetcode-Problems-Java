5247fe31224865330358867ee4960729c676f802
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-08 11:49:09 +0500

    Time: 0 ms (100.00%) | Memory: 40.8 MB (67.00%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,19 +1,17 @@
 class Solution {
-    public int solve(int[] nums, int ind, int[] dp){
-        if(ind == 0){
-            return nums[ind];
-        }
-        if(ind < 0){
-            return 0;
-        }
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
+    public int solve(int[] nums){
+        int n = nums.length;
+        int prev1 = nums[0];
+        int prev2 = 0;
 
-        int pick = nums[ind] + solve(nums, ind-2, dp);
-        int notPick = solve(nums, ind-1, dp);
+        for(int i=1; i<n; i++){
+            prev2 = nums[i] + prev2;
 
-        return dp[ind] = Math.max(pick, notPick);
+            int curr = Math.max(prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 
     public int rob(int[] nums) {
@@ -30,13 +28,6 @@ class Solution {
             temp2[i-1] = nums[i];
         }
 
-        int[] dp = new int[n-1];
-        Arrays.fill(dp, -1);
-        int ans1 = solve(temp1, n-2, dp);
-
-        Arrays.fill(dp, -1);
-        int ans2 = solve(temp2, n-2, dp);
-
-        return Math.max(ans1, ans2);
+        return Math.max(solve(temp2), solve(temp1));
     }
 }
