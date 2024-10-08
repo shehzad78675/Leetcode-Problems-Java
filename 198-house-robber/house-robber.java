636201b94c5ab10d66274384c32efae15908dbdb
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-08 10:52:53 +0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (25.92%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,26 +1,18 @@
 class Solution {
-    public int solve(int[] nums, int ind, int[] dp){
-        if(ind == 0){
-            return nums[ind];
-        }
-
-        if(ind < 0){
-            return 0;
-        }
-
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
-        int pickIn = nums[ind] + solve(nums, ind-2, dp);
-        int notPickIn = 0 + solve(nums, ind-1, dp);
-
-        return dp[ind] = Math.max(pickIn, notPickIn);
-    }
-
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return solve(nums, n-1, dp);
+
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int pick = nums[i];
+            if(i > 1){
+                pick += dp[i-2];
+            }
+            int notPick = dp[i-1];
+
+            dp[i] = Math.max(pick, notPick);
+        }
+        return dp[n-1];
     }
 }
