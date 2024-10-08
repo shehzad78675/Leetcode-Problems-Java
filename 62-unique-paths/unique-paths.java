fad3210d975ec73bbcf5d8d202c2725984962d9c
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-08 21:36:24 +0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (77.48%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,20 +1,25 @@
 class Solution {
-    public int solve(int i, int j, int[][] dp){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i < 0 || j < 0){
-            return 0;
-        }
-
-        if(dp[i][j] != 0){
-            return dp[i][j];
-        }
-
-        return dp[i][j] = solve(i-1, j, dp) + solve(i, j-1, dp);
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return solve(m-1, n-1, dp);
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = 1;
+                }else{
+                    int up = 0;
+                    if(i > 0){
+                        up = dp[i-1][j];
+                    }
+
+                    int left = 0;
+                    if(j > 0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
