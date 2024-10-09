9b42334633edb657b574fced16221da897b09d8a
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-09 07:46:55 +0500

    Time: 6 ms (12.79%) | Memory: 47.6 MB (24.86%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,27 +1,26 @@
 class Solution {
-    public int solve(int grid[][], int i, int j, int[][] dp){
-        if(i == 0 && j == 0){
-            return grid[i][j];
-        }
-
-        if(i < 0 || j < 0){
-            return (int) 1e9;
-        }
-
-        if(dp[i][j] != 0){
-            return dp[i][j];
-        }
-
-        int up = grid[i][j] + solve(grid, i-1, j, dp);
-        int left = grid[i][j] + solve(grid, i, j-1, dp);
-
-        return dp[i][j] = Math.min(up, left);
-    }
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
 
         int[][] dp = new int[n][m];
-        return solve(grid, n-1, m-1, dp);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }else{
+                    int up = grid[i][j];
+                    int left = grid[i][j];
+
+                    if(i>0) up += dp[i-1][j];
+                    else up += (int) 1e9;
+                    if(j>0) left += dp[i][j-1];
+                    else left += (int) 1e9;
+
+                    dp[i][j] = Math.min(up, left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
