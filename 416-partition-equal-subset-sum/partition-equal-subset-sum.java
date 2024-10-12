db2522671fcfc633b69466716dbfd3321aa57706
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-12 09:59:03 +0500

    Time: 45 ms (52.84%) | Memory: 44.6 MB (80.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean subsetSum(int[] nums, int k){
         int n = nums.length;
-        boolean[][] dp = new boolean[n][k+1];
+        boolean[] dp = new boolean[k+1];
 
-        for(int i=0; i<n; i++){
-            dp[i][0] = true;
-        }
+        dp[0] = true;
 
         if(nums[0] <= k){
-            dp[0][nums[0]] = true;
+            dp[nums[0]] = true;
         }
 
         for(int i=1; i<n; i++){
+            boolean temp[] = new boolean[k+1];
+            temp[0] = true;
             for(int target=1; target<=k; target++){
-                boolean notPick = dp[i-1][target];
+                boolean notPick = dp[target];
                 boolean pick = false;
-                if(nums[i] <= target) pick = dp[i-1][target-nums[i]];
+                if(nums[i] <= target) pick = dp[target-nums[i]];
 
-                dp[i][target] = pick || notPick;
+                temp[target] = pick || notPick;
             }
+            dp = temp;
         }
-        return dp[n-1][k];
+        return dp[k];
     }
     public boolean canPartition(int[] nums) {
         int n = nums.length;
