af1e3de322a753824c524f8c82763aa83bf8abcc
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-15 10:23:22 +0500

    Time: 15 ms (79.66%) | Memory: 44.9 MB (32.65%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,29 +1,26 @@
 class Solution {
-    public int solve(int ind, int[] coins, int amount, int[][] dp){
-        if(ind == 0){
-            if(amount%coins[ind] == 0) return amount/coins[ind];
-            return (int) 1e9;
-        }
-
-        if(dp[ind][amount] != -1){
-            return dp[ind][amount];
-        }
-
-        int notTake = solve(ind-1, coins, amount, dp);
-        int take = Integer.MAX_VALUE;
-        if(coins[ind] <= amount) take = 1 + solve(ind, coins, amount-coins[ind], dp);
-
-        return dp[ind][amount] = Math.min(take, notTake);
-    }
-    
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
         int[][] dp = new int[n][amount+1];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
+        for(int t=0; t<=amount; t++){
+            if(t%coins[0] == 0){
+                dp[0][t] = t/coins[0];
+            }else{
+                dp[0][t] = (int) 1e9;
+            }
         }
-        int res = solve(n-1, coins, amount, dp);
+
+        for(int i=1; i<n; i++){
+            for(int target=0; target<=amount; target++){
+                int notTake = dp[i-1][target];
+                int take = Integer.MAX_VALUE;
+                if(coins[i] <= target) take = 1 + dp[i][target-coins[i]];
+
+                dp[i][target] = Math.min(notTake, take);
+            }
+        }
+        int res = dp[n-1][amount];
         return res >= (int) 1e9 ? -1 : res;
     }
 }
