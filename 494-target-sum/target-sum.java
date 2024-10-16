495f6312be60a85e0e5d7f134375916f65fd281f
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-16 07:57:35 +0500

    Time: 4 ms (94.06%) | Memory: 42.7 MB (57.97%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int solve(int ind, int[] nums, int k){
+    public int solve(int ind, int[] nums, int k, int[][] dp){
         if(ind == 0){
             if(k == 0 && nums[0] == k) return 2;
             if(k == 0 || nums[0] == k) return 1;
             return 0;
         }
 
-        int notTake = solve(ind-1, nums, k);
-        int take = 0;
-        if(nums[ind] <= k) take = solve(ind-1, nums, k-nums[ind]);
+        if(dp[ind][k] != -1){
+            return dp[ind][k];
+        }
 
-        return take + notTake;
+        int notTake = solve(ind-1, nums, k, dp);
+        int take = 0;
+        if(nums[ind] <= k) take = solve(ind-1, nums, k-nums[ind], dp);
+
+        return dp[ind][k] = take + notTake;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int totalSum = 0;
@@ -25,6 +29,10 @@ class Solution {
         }
 
         int k = (totalSum-target)/2;
-        return solve(n-1, nums, k);
+        int[][] dp = new int[n][k+1];
+        for(int i=0; i<n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(n-1, nums, k, dp);
     }
 }
