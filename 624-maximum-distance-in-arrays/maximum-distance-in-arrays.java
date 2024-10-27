7f7a04239f8a6755b214099f51a91779c5ba4fd3
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-27 09:25:38 +0500

    Time: 12 ms (16.33%) | Memory: 64.7 MB (29.66%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int res = Integer.MIN_VALUE;
+        int n = arrays.size();
+
+        int min = arrays.get(0).get(0);
+        int max = arrays.get(0).get(arrays.get(0).size()-1);
+
+        for(int i=1; i<n; i++){
+            res = Math.max(res, Math.abs(arrays.get(i).get(0)-max));
+            res = Math.max(res, Math.abs(arrays.get(i).get(arrays.get(i).size()-1) - min));
+
+            min = Math.min(min, arrays.get(i).get(0));
+            max = Math.max(max, arrays.get(i).get(arrays.get(i).size()-1));
+        }
+
+        return res;
+    }
+}
